313e711cb0a73686e9f7f3c58b6bd5e2110b5455
leandrosouzaa <leandror.dev@gmail.com>
2020-02-05 14:46:38 -0300

    Inserção de comentários.

--- Hello World/HelloWorld.c
+++ Hello World/HelloWorld.c
@@ -1,13 +1,19 @@
-// Para incluir uma bibltoeca padrão utiliza-se '<>' entre o nome da bibloteca.
-// A bibloteca 'stdio' é a bibloteca padrão para input e output.
+/* 
+ Para incluir uma bibltoeca padrão utiliza-se '<>' entre o nome da bibloteca.
+ A bibloteca 'stdio' é a bibloteca padrão para input e output.
+*/
 #include <stdio.h>
 
-// O método main é o primeiro método a ser executado no programa.
+/* O método main é o primeiro método a ser executado no programa. */
 int main() {
 
-    // Utilizando o método printf contido na bibloteca stdio. Ele é utilizado para imprimir uma informação em tela. 
-    // O printf imprime esta informação formatada
+    /* 
+     Utilizando o método printf contido na bibloteca stdio. Ele é utilizado para imprimir uma informação em tela. 
+     O printf imprime esta informação formatada
+    */
+    
     printf("Hello World!");
 
-    // É padrão retornar alguma informação mesmo quando não é necessário.
+    /* É padrão retornar alguma informação mesmo quando não é necessário.*/
+    return 0;
 }
