62f97a7831e4ad04e6415fa089f37eff7dbe0f8e
leandrosouzaa <leandror.dev@gmail.com>
2020-02-05 15:03:17 -0300

    Conceitos e caracteristicas de variáveis.

---
+++ Variáveis/Variaveis.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    /* 
+    Por padrão é comum definir um valor de variável assim que a mesma é criada.
+    Caso contrário, o compilador gera um valor aleatório para ela. 
+    Não é possível mudar o valor o tipo da variável durante a execução do código. 
+    4 caracteristicas basicas de uma variável
+        1) Toda variável possui um nome;
+        2) Toda variável possui um tipo;
+        3) Toda variável possui um tamanho;
+        4) Toda variável possui um valor
+        
+    Informações sobre variáveis
+        a) O seu armazenamento é temporário;
+        b) Quando o programa for fechado, tudo que estiver na memória é apagado;
+        c) valores armazenados na memórias são voláteis;
+        d) Os valores da variáveis sao sobrepostos, ou seja, ao definir um novo valor, o valor antigo é perdido.
+    */
+    
+    return 0;
+
+
+}
