9f43f75e2b15b99b03bb1d2b9372c5406c8f0670
leandrosouzaa <leandror.dev@gmail.com>
2020-02-05 21:18:49 -0300

    Operações Matemáticas

---
+++ Operações Matemáticas/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+
+    int x, y;
+    x = 15;
+    y = 20;
+
+    int soma = x + y;
+    printf("O resultado da soma é %i\n" ,soma);
+
+    int sub = x - y;
+    printf("O resultado da subtração é %i\n", sub);
+
+    int mult = x * y;
+    printf("O resultado da multiplicação é %i\n", mult);
+
+    int div = y / 3;
+    printf("O resultado da divisão é %i\n", div);
+
+}
