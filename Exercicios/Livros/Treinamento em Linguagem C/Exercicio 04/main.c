386c1d81f2ce9f0c817d767d3b37cc81eb6d9ee0
leandrosouzaa <leandror.dev@gmail.com>
2020-03-06 08:50:43 -0300

    Exercicio 04

---
+++ Exercicios/Livros/Treinamento em Linguagem C/Exercicio 04/main.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main() {
+
+    printf("\n\t Bom dia! Shirley.");
+    printf("Você já tomou café?\n");
+    printf("\n\nA solução não existe!\nNão insista!");
+
+     getchar();
+     return 0;
+ }
