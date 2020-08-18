078568e1d0f0e5fa7eb1b6147cc4f68d305fe4ea
leandrosouzaa <leandror.dev@gmail.com>
2020-08-18 20:58:06 -0300

    ex 1

---
+++ Exercicios/Listas/17-08-2020/1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+   printf("Padaria Hotpão - Leandro Ribeiro de Souza \n\n");
+
+   int qtdBroa, qtdPao;
+   float valorTotal, valorPoupado, valorFinal, porcentagem;
+
+   printf("Informe a quantidade de Broas vendidas: ");
+   scanf("%i", &qtdBroa);
+
+   printf("Informe a quantidade de pães vendidos: ");
+   scanf("%i", &qtdPao);
+
+   printf("Informe a porcentagem das vendas que deseja poupar: ");
+   scanf("%f", &porcentagem);
+
+   valorTotal = (qtdBroa*4.50 + qtdPao*0.6);
+   valorPoupado = (valorTotal * (porcentagem/100));
+   valorFinal = (valorTotal - valorPoupado);
+
+   printf("\nQuantidade de Broas Vendidas: %i\n.", qtdBroa);
+   printf("Quantidade de Pães Vendidos: %i.\n", qtdBroa);
+   printf("Total arrecadado com as vendas: R$%g.\n", valorTotal);
+   printf("Valor poupado (%g%%): R$%g.\n", porcentagem, valorPoupado);
+   printf("Valor restado em Caixa: R$%g.", valorFinal);
+
+   return 0;
+}
+
