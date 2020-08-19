57f346ec328bdb8471bbea48bac529c518b99b04
leandrosouzaa <leandror.dev@gmail.com>
2020-08-18 21:16:56 -0300

    ex 3

---
+++ Exercicios/Listas/17-08-2020/3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+   printf("Três amigos - Leandro Ribeiro de Souza \n\n");
+
+   int valorSemCentavos = 0;
+   float valorDaCorrida, valorComCentavos = 0;
+
+   printf("Informe o valor total da corrida: R$");
+   scanf("%f", &valorDaCorrida);
+
+   valorSemCentavos = valorDaCorrida / 3;
+   valorComCentavos = ((valorDaCorrida / 3) - valorSemCentavos) * 2 + valorDaCorrida / 3;
+
+   printf("Valor a ser pago pelo dois primeiros: R$%i\n", valorSemCentavos);
+   printf("Valor a ser pago pelo ultimo: R$%g\n", valorComCentavos);
+
+   return 0;
+}
