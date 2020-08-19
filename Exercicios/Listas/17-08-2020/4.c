49970b7b44511a00c723926690cb7fca5eeac393
leandrosouzaa <leandror.dev@gmail.com>
2020-08-18 21:28:33 -0300

    ex 4

---
+++ Exercicios/Listas/17-08-2020/4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+   printf("Lanchonete Gostosura - Leandro Ribeiro de Souza \n\n");
+
+   int qtdSanduiche = 0;
+   float qQueijo, qPresunto, qHamburguer = 0;
+
+   printf("Informe a quantidade de sanduíches que deseja fazer: ");
+   scanf("%i", &qtdSanduiche);
+
+   qQueijo = qtdSanduiche * 0.05 * 2;
+   qPresunto = qtdSanduiche * 0.05;
+   qHamburguer = qtdSanduiche * 0.1;
+
+   printf("\nQuantidade de ingredientes necessários para o recheio de %i sanduíches:\n", qtdSanduiche);
+   printf("Queijo: %gkg\n",qQueijo );
+   printf("Presunto: %gkg\n",qPresunto );
+   printf("Hamburguer: %gkg\n",qHamburguer );
+
+   return 0;
+}
