49712dd7ec8f80d6b94dc07aae475fc963a8d5bc
leandrosouzaa <leandror.dev@gmail.com>
2020-08-26 20:34:21 -0300

    ex 2

---
+++ Exercicios/Listas/24-08-2020/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   printf("Maior número - Leandro Ribeiro de Souza \n\n");
+
+   float num1, num2 = 0;
+
+   printf("Informe um número: ");
+   scanf("%f", &num1);
+
+   printf("Informe outro número: ");
+   scanf("%f", &num2);
+
+   if (num1 > num2) {
+      printf("\nO número %0.2f é maior que o número %0.2f.", num1, num2);
+   } else {
+      if (num2 > num1) {
+         printf("\nO número %0.2f é maior que o número %0.2f.", num2, num1);
+      } else {
+         printf("\nO número %0.2f é igual ao número %0.2f.", num2, num1);
+      };
+   };
+
+   return 0;
+}
