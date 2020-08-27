a0ac699dd9595bff64dd1e6e6d9922527c99801c
leandrosouzaa <leandror.dev@gmail.com>
2020-08-26 21:55:06 -0300

    ex 8

---
+++ Exercicios/Listas/24-08-2020/8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+   printf("Cálculo de Média - Leandro Ribeiro de Souza \n\n");
+
+   float n1,n2,n3,media = 0;
+
+   printf("Informe a nota 1: ");
+   scanf("%f", &n1);
+
+   printf("Informe a nota 2: ");
+   scanf("%f", &n2);
+
+   printf("Informe a nota 3: ");
+   scanf("%f", &n3);
+
+   media = (n1 + n2 + n3) / 3;
+
+   printf("\nMédia do Aluno: %0.2f.\n", media);
+
+   if (media < 6) {
+      printf("Situação: REPROVADO");
+   } else {
+      printf("Situação: APROVADO");
+   };
+
+   return 0;
+}
