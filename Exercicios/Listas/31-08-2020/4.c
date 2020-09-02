071c61b37eec6fd1f16ca2916d5da1a499e8f52f
leandrosouzaa <leandror.dev@gmail.com>
2020-09-02 18:32:51 -0300

    ex 4

---
+++ Exercicios/Listas/31-08-2020/4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   printf("Números entre dois números - Leandro Ribeiro de Souza \n\n");
+
+   int i, num1, num2;
+
+   printf("Informe um valor inteiro: ");
+   scanf("%i", &num1);
+
+   printf("Informe outro valor inteiro: ");
+   scanf("%i", &num2);
+
+   if (num1 < num2) {
+      for(i=num1; i <= num2; i++) {
+         printf("%i\n", i);
+      };
+   } else {
+      for(i=num2; i <= num1; i++) {
+         printf("%i\n", i);
+      };
+   }
+
+   return 0;
+}
