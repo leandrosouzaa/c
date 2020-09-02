4983a924189b738c7635c51254ca52c86ef7381d
leandrosouzaa <leandror.dev@gmail.com>
2020-09-02 16:23:38 -0300

    ex 1

---
+++ Exercicios/Listas/31-08-2020/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   printf("0 à 30 - Leandro Ribeiro de Souza \n\n");
+
+   int i;
+
+   for(i=0; i<=30; i+=2) {
+      printf("%i\n", i);
+   };
+
+   for(i=29; i > 0; i--) {
+      printf("%i\n", i);
+   };
+
+   return 0;
+}
