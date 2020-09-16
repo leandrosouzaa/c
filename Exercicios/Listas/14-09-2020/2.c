b7e72ec46768eeeb5f874be52a9b37fee31617a6
leandrosouzaa <leandror.dev@gmail.com>
2020-09-15 22:38:59 -0300

    att ex 2

--- Exercicios/Listas/14-09-2020/2.c
+++ Exercicios/Listas/14-09-2020/2.c
@@ -4,16 +4,20 @@ int main() {
    printf("João das Contas - Leandro Ribeiro de Souza \n\n");
 
    float valorDoado, totalArrecadado = 0;
+   int totalDoadores = 0;
 
    while (totalArrecadado < 20000) {
       printf("Informe o valor doado: R$");
       scanf("%f", &valorDoado);
 
       totalArrecadado+= valorDoado;
+      totalDoadores++;
 
       printf("Valor Arrecadado: R$%0.2f.\n\n", totalArrecadado);
    }
 
+   printf("RELATÓRIO DE DOADORES\n");
+   printf("Total de doadores: %i.\n", totalDoadores);
    printf("Valor final arrecadado: R$%0.2f.", totalArrecadado);
 
    return 0;
