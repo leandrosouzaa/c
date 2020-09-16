6b7b241067ff59b0d6542e3a92488703b87a6856
leandrosouzaa <leandror.dev@gmail.com>
2020-09-15 22:15:50 -0300

    ex 4

---
+++ Exercicios/Listas/14-09-2020/4.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+   printf("Eleições Municipais - Leandro Ribeiro de Souza \n\n");
+
+   int voto, votosAlibaba = 0, votosAlcapone = 0, votosBrancos = 0, votosNulos = 0;
+
+   do {
+      printf("Número dos Candidatos\n");
+      printf("Alibabá: 83.\n");
+      printf("Alcapone: 93.\n");
+      printf("Branco: 00.\n");
+      printf("Para sair digite -1.\n");
+      printf("Nulo: Qualquer outra opção.");
+
+      printf("Número do Candidato: ");
+      scanf("%i", &voto);
+
+      printf("\n\n");
+
+      switch (voto) {
+         case 83:
+            votosAlibaba++;
+            break;
+         case 93:
+            votosAlcapone++;
+            break;
+         case 0:
+            votosBrancos++;
+            break;
+         case -1:
+            break;      
+         default:
+            votosNulos++;
+            break;
+      }
+   } while (voto != -1);
+
+   printf("RELATÓRIO FINAL\n");
+   printf("Alibabá: %i votos.\n", votosAlibaba);
+   printf("Alcapone: %i votos.\n", votosAlcapone);
+   printf("Votos Brancos: %i votos.\n", votosBrancos);
+   printf("Votos Nulos: %i votos.\n", votosNulos);
+
+   if (votosAlibaba > votosAlcapone) {
+      printf("VENCEDOR: Alibabá.");
+   } else {
+      if (votosAlcapone > votosAlibaba) {
+         printf("VENCEDOR: Alcapone.");
+      } else {
+         printf("VENCEDOR: Não Houve");
+      }
+   }
+
+   return 0;
+}
