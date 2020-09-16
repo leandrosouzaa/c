6078618ef4e0416014cb358841aad49b5fa9f104
leandrosouzaa <leandror.dev@gmail.com>
2020-09-15 22:51:07 -0300

    att ex 1

--- Exercicios/Listas/14-09-2020/1.c
+++ Exercicios/Listas/14-09-2020/1.c
@@ -4,6 +4,7 @@ int main() {
    printf("Caixa de Mercado - Leandro Ribeiro de Souza \n\n");
 
    float valorProduto, total;
+   int qtdProdutos = -1;
 
    do {
       printf("Digite 0 para Sair.\n");
@@ -13,9 +14,11 @@ int main() {
       total = valorProduto + total;
       printf("Atual valor da compra: %0.2f\n\n", total);
 
+      qtdProdutos++;
    } while (valorProduto > 0);
 
    printf("EXTRATO DA COMPRA\n");
+   printf("Total de Produtos: %i.\n", qtdProdutos);
    printf("Valor final da Compra: R$%0.2f.", total);
 
    return 0;
