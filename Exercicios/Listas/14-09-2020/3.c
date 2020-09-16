e09d9c57894bc0245feb4020151eb9397d432bd2
leandrosouzaa <leandror.dev@gmail.com>
2020-09-15 22:34:12 -0300

    att no ex 3

--- Exercicios/Listas/14-09-2020/3.c
+++ Exercicios/Listas/14-09-2020/3.c
@@ -3,16 +3,18 @@
 int main() {
    printf("Festa das Nações - Leandro Ribeiro de Souza \n\n");
 
-   float totalCaixa = 0, totalTemp;
+   float totalCaixa = 0, totalTemp, valorTotalFichas2, valorTotalFichas5;
    char continua;
-   int qtdFichas2, qtdFichas5;
+   int qtdFichas2, qtdFichas5, totalFichas2 = 0, totalFichas5 = 0, qtdClientes = 0;
 
    do {
       printf("Informe o total de fichas de R$2.00: ");
       scanf("%i", &qtdFichas2);
+      totalFichas2+= qtdFichas2;
 
       printf("Informe o total de fichas de R$5.00: ");
       scanf("%i", &qtdFichas5);
+      totalFichas5+= qtdFichas5;
 
       totalTemp = qtdFichas2 * 2 + qtdFichas5 * 5;
       totalCaixa+= totalTemp;
@@ -23,11 +25,20 @@ int main() {
       printf("Existem mais pessoas na fila (S para sim e N para não)?: ");
       scanf(" %c", &continua);
       printf("\n");
+      qtdClientes++;
 
    } while (continua == 'S');
 
+   valorTotalFichas2 = qtdFichas2 * 2;
+   valorTotalFichas5 = qtdFichas5 * 5;
+
    printf("RELATÓRIO DE VENDAS\n");
-   printf("Valor total de Vendas: R$%0.2f.", totalCaixa);
+   printf("Total de clientes: %i.\n", qtdClientes);
+   printf("Total de fichas de R$2.00 vendidas: %i.\n", qtdFichas2);
+   printf("Total vendido em fichas de R$2.00: R$%0.2f.\n", valorTotalFichas2);
+   printf("Total de fichas de R$5.00 vendidas: %i\n", qtdFichas5);
+   printf("Total vendido em fichas de R$5.00: R$%0.2f.\n", valorTotalFichas5);
+   printf("Valor total de Vendas: R$%0.2f.\n", totalCaixa);
 
    return 0;
 }
