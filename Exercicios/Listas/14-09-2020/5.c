ca52fe934d13a53e91a42a0146c26f11f3766c3a
leandrosouzaa <leandror.dev@gmail.com>
2020-09-16 22:48:01 -0300

    att ex 5

--- Exercicios/Listas/14-09-2020/5.c
+++ Exercicios/Listas/14-09-2020/5.c
@@ -3,7 +3,7 @@
 int main() {
    printf("Entrada do Show - Leandro Ribeiro de Souza \n\n");
 
-   int idade, qtdMenores = 0, qtdIdosos = 0, qtdOutros = 0;
+   int idade, qtdMenores = 0, qtdIdosos = 0, qtdOutros = 0, qtdPessoas = 0;
 
    do {
       printf("\nDigite 0 para Sair.\n");
@@ -12,18 +12,22 @@ int main() {
 
       if ( idade > 0 && idade < 18) {
          qtdMenores++;
+         qtdPessoas++;
       } else {
          if (idade > 60){
             qtdIdosos++;
+            qtdPessoas++;
          } else {
             if (idade >= 18 && idade <= 60) {
                qtdOutros++;
+               qtdPessoas++;
             }
          }
       }
    } while (idade > 0);
 
    printf("RELATÓRIO FINAL\n");
+   printf("Total de Pessoas: %i.\n", qtdPessoas);
    printf("Pessoas com menos de 18 anos: %i.\n", qtdMenores);
    printf("Pessoas com mais de 60 anos: %i.\n", qtdIdosos);
    printf("Pessoas entre 18 anos e 60 anos: %i.\n", qtdOutros);
