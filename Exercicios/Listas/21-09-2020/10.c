9839ce5d96d1cea2e430753de5432df054369421
leandrosouzaa <leandror.dev@gmail.com>
2020-09-23 22:58:28 -0300

    ex 10

---
+++ Exercicios/Listas/21-09-2020/10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+   printf("Contas de uma Rua - Leandro Ribeiro de Souza \n\n");
+
+   int i =0, indexMenorConta;
+   float contas[25], total = 0, icms, valorMaiorConta ;
+
+   for (i=0; i < 10; i++) {
+      printf("Conta da Casa %i: R$", i + 1);
+      scanf("%f", &contas[i]);
+   }
+
+   total = contas[0];
+   valorMaiorConta = contas[0];
+   indexMenorConta = 0;
+   for (i=1; i < 10; i++) {
+      total+= contas[i];
+
+      if (contas[i] > valorMaiorConta) {
+         valorMaiorConta = contas[i];
+      }
+
+      if (contas[indexMenorConta] > contas[i]) {
+         indexMenorConta = i;
+      }
+   }
+   icms = total * 0.25;
+
+   printf("\nRELATÓRIO FINAL\n");
+   printf("Casa que menos gastou Energia: %i.\n", indexMenorConta + 1);
+   printf("Valor da conta mais cara: R$%0.2f.\n", valorMaiorConta);
+   printf("Total das contas: R$%0.2f.\n", total);
+   printf("Valor do ICMS: %0.2f.\n", icms);
+
+   return 0;
+}
