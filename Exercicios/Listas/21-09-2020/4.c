9c98cdf3d5629dc366aeafc2e70395af42fa9693
leandrosouzaa <leandror.dev@gmail.com>
2020-09-30 22:50:30 -0300

    refatoração dos exercicios

--- Exercicios/Listas/21-09-2020/4.c
+++ Exercicios/Listas/21-09-2020/4.c
@@ -3,15 +3,15 @@
 int main() {
    printf("Média dos Alunos - Leandro Ribeiro de Souza \n\n");
 
-   int i, notas[10], soma = 0, qtdAcima = 0;
-   float media;
+   int i, qtdAcima = 0;
+   float notas[10], soma = 0, media;
 
    for (i=0; i < 10; i++) {
       printf("Nota do Aluno %i: ", i + 1);
-      scanf("%i", &notas[i]);
+      scanf("%f", &notas[i]);
 
       soma+=notas[i];
-   }
+   }  
 
    media = soma / 10;
 
@@ -22,7 +22,7 @@ int main() {
    }
 
    printf("\nRELATÓRIO FINAL\n");
-   printf("Soma das Notas: %i.\n", soma);
+   printf("Soma das Notas: %0.2f.\n", soma);
    printf("Média da Sala: %0.2f.\n", media);
    printf("Alunos acima da média: %i.", qtdAcima);
 
