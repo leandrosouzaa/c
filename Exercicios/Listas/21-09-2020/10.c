12f612a3559aa1962738e477bc70e3a7895665f2
leandrosouzaa <leandror.dev@gmail.com>
2020-10-01 23:33:01 -0300

    refactor do ex 10

--- Exercicios/Listas/21-09-2020/10.c
+++ Exercicios/Listas/21-09-2020/10.c
@@ -4,7 +4,7 @@ int main() {
    printf("Contas de uma Rua - Leandro Ribeiro de Souza \n\n");
 
    int i =0, indexMenorConta;
-   float contas[25], total = 0, icms, valorMaiorConta ;
+   float contas[25], total = 0, icms, valorMaiorConta, icmsIndividual;
 
    for (i=0; i < 25; i++) {
       printf("Conta da Casa %i: R$", i + 1);
@@ -31,7 +31,12 @@ int main() {
    printf("Casa que menos gastou Energia: %i.\n", indexMenorConta + 1);
    printf("Valor da conta mais cara: R$%0.2f.\n", valorMaiorConta);
    printf("Total das contas: R$%0.2f.\n", total);
-   printf("Valor do ICMS: %0.2f.\n", icms);
+   printf("Valor total pago ICMS: %0.2f.\n", icms);
+   printf("\nICMS POR CASA\n");
+   for (i=0; i < 25; i++) {
+      icmsIndividual = contas[i] * 0.25;
+      printf("ICMS pago pela casaa %d: R$%0.2f.\n", i + 1, icmsIndividual);
+   }
 
    return 0;
 }
