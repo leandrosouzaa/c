28befc301b4c2b7499ff496a11b6d92937662205
leandrosouzaa <leandror.dev@gmail.com>
2020-10-19 20:27:55 -0300

    add points system

--- Exercicios/Diversos/Jogo da Velha/main.c
+++ Exercicios/Diversos/Jogo da Velha/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+typedef struct {
+   int o , x, tie;
+} Points;
+
 char squares[9] = {'1','2','3','4','5','6','7','8', '9'};
 int lines[8][3] = {
    {1,2,3},
@@ -12,26 +16,43 @@ int lines[8][3] = {
    {0,4,8},
    {2,4,6}
 };
-int i = 0, position, roundGame = 0, iWinner, iReset;
-
+int i = 0, position, roundGame = 0, iWinner, iReset, iTie;
+Points points  = {
+   .o = 0,
+   .x = 0,
+   .tie = 0
+};
 
 int printBoard() {
    printf("\e[1;1H\e[2J");
    printf("\n");
    printf(" %c | %c | %c\n", squares[0] , squares[1] , squares[2] );
-   printf("---+---+---\n");
+   printf("———+———+———\n");
    printf(" %c | %c | %c\n", squares[3], squares[4], squares[5]);
-   printf("---+---+---\n");
+   printf("———+———+———\n");
    printf(" %c | %c | %c\n", squares[6], squares[7], squares[8]);
    
    return 0;
 }
 
 int reset() {
+   char a;
+
    for (iReset = 0; iReset < 9; iReset++) {
-      squares[iReset] = ' ';
+      sprintf(&a, "%d", iReset + 1);
+      squares[iReset] = a;
    }
    roundGame = 0;
+
+   return 0;
+}
+
+int showPoints() {
+   printf("+———————————————+\n");
+   printf("| X | O | Velha |\n");
+   printf("|———+———+———————|\n");
+   printf("| %d | %d | %s %d   |\n", points.x, points.o, points.tie >= 10 ? "" : " " ,points.tie);
+   printf("+———————————————+\n");
 
    return 0;
 }
@@ -39,13 +60,30 @@ int reset() {
 int showRoundWinner(char winner) {
    printf("\e[1;1H\e[2J\n");
    printBoard();
-   printf("\nVencedor: %c", winner);
+   printf("\nVencedor: %c\n", winner);
+   if (winner == 'X') {
+      points.x++;
+   } else {
+      points.o++;
+   }
+   printf("Pontuação até o momento:\n");
+   showPoints();
+
+   return 0;
+}
+
+int showTie() {
+   printf("\e[1;1H\e[2J\n");
+   printBoard();
+   printf("\nDeu velha!\n");
+   points.tie++;
+   printf("Pontuação até o momento:\n");
+   showPoints();
 
    return 0;
 }
 
 char checkWinner() {
-   printf("Chamou");
    for (iWinner = 0; iWinner < 8; iWinner++) {
       if (squares[lines[iWinner][0]] && squares[lines[iWinner][0]] == squares[lines[iWinner][1]] && squares[lines[iWinner][0]] == squares[lines[iWinner][2]] ) {
          i = 10;
@@ -77,14 +115,18 @@ int readSquare() {
 int main() {
    char repeat;
 
-   printf("Jogo da Velha - Leandro Ribeiro de Souza \n\n");
+   printf("Jogo da Velha – Leandro Ribeiro de Souza \n\n");
    do {
+      i = 0;
       do {
          readSquare();
          checkWinner();
          i++;
       } while(i < 9);
-      printf("\nJogar Novamente? (S ou N)");
+      if (i==9) {
+         showTie();
+      }
+      printf("\nJogar Novamente? (S ou N): ");
       scanf(" %c", &repeat);
       if (repeat == 'S') {
          reset();
