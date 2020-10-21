089d4b519032119094181a4a0ad0d42a5f97d324
leandrosouzaa <leandror.dev@gmail.com>
2020-10-20 22:07:13 -0300

    ex 2

---
+++ Exercicios/Listas/2º Semestre/19-10-2020/2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+   printf("Placa Mercosul - Leandro Ribeiro de Souza \n\n");
+
+   int tl = 0, i, qtdNumeros = 0, qtdLetras = 0, indexPrimeiro = -1, indexUltimo;
+   char placa[7];
+
+   printf("Informe uma placa do Mercosul: ");
+   do {
+      scanf("%c", &placa[tl]);
+      tl++;
+   } while(tl < 7);
+   if (tl < 7 ) 
+      tl--;
+
+   for(i=0; i < tl; i++) {
+      if (placa[i] >='0' && placa[i] <= '9') {
+         qtdNumeros++;
+         if (indexPrimeiro == -1) {
+            indexPrimeiro = i;
+         }
+         indexUltimo = i;
+      } else {
+         qtdLetras++;
+      }
+   }
+
+   printf("\nRELATÓRIO FINAL\n");
+   printf("Quantidade de Letras: %d.\n", qtdLetras);
+   printf("Quantidade de Números: %d.\n", qtdNumeros);
+   printf("Posição do primeiro número: %d.\n", indexPrimeiro + 1);
+   printf("Números da placa: ");
+   for(i=0; i < tl; i++) {
+      if (placa[i] >='0' && placa[i] <= '9') 
+         printf("%c", placa[i]);
+   }
+   printf("\nLetras da placa: ");
+   for(i=0; i < tl; i++) {
+      if (!(placa[i] >='0' && placa[i] <= '9')) 
+         printf("%c ", placa[i]);
+   }
+
+
+   return 0;
+}
