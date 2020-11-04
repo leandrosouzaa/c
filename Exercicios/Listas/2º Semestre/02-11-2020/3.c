dea63df9030f74b8ef2ea1f61bf2eeebd3facf96
leandrosouzaa <leandror.dev@gmail.com>
2020-11-04 11:13:26 -0300

    ex 3

--- Exercicios/Listas/2º Semestre/02-11-2020/3.c
+++ Exercicios/Listas/2º Semestre/02-11-2020/3.c
@@ -3,33 +3,43 @@
 int main() {
    printf("Página de um Livro - Leandro Ribeiro de Souza \n\n");
 
-   char temp,pagina[40][80] = {' '};
-   int  tl, i, j,full, qtdVogais = 0, qtdConsoantes = 0, qtdPalavras;
-   printf("\nDIGITE A PÁGINA ABAIXO\n\n");
-   for (i=0; i < 40; i++) {
+   char temp,pagina[3][10] = {' '};
+   int  tl, i, j,full, qtdVogais = 0, qtdConsoantes = 0, qtdPalavras = 0, tempCounter = 0;
+   printf("\nDIGITE SEU TEXTO:\n\n");
+   for (i=0; i < 3; i++) {
       tl=0;
-      printf("%i |", i + 1);
+      printf("%s%d |", i < 9 ? "0" : "",i + 1);
       do {
-         scanf(" %c", &pagina[i][tl]);
-      } while(tl < 80 && pagina[i][tl++] != 10);
-      if (tl < 80) 
+         scanf("%c", &pagina[i][tl]);
+      } while(tl < 10 && pagina[i][tl++] != 10);
+      if (tl < 10) 
          tl--;
    }
 
-   for(i=0; i < 40; i++) {
-      for(j=0; j < 80; j++) {
-         temp = pagina[i][j];
+   printf("\nTEXTO DIGITADO: \n\n");
+   for(i=0; i < 3; i++) {
+      tempCounter = 0;
+      if (i > 0 && pagina[i-1][9] != NULL) {
          printf("\n");
+      }
+      printf("%s%d |", i < 9 ? "0" : "",i + 1);
+      for(j=0; j < 10; j++) {
+
+         temp = pagina[i][j];
          if (temp==65||temp==69||temp==79||temp==73||temp==85||temp==97||temp==101||temp==105||temp==111) {
             qtdVogais++;
+            tempCounter++;
          } else {
             if ((temp > 64 && temp < 91) || (temp > 96 && temp < 123)) {
                qtdConsoantes++;
+               tempCounter++;
             }
          }
-         if ((pagina[i][j] != ' ' && pagina[i][j + 1] == ' ')) {
+         if ((pagina[i][j] != ' ' && (pagina[i][j + 1] == ' '|| pagina[i][j] == 10 || tempCounter == 10))) {
             qtdPalavras++;
+
          }
+
          printf("%c", temp);
       }
    }
