da15996484097e7380e936c83f2d9ba413af5f31
Leandro Souza <leandror.dev@gmail.com>
2020-11-24 16:36:47 -0300

    Update 2.c

--- Exercicios/Listas/2º Semestre/23-11-2020-AOC/2.c
+++ Exercicios/Listas/2º Semestre/23-11-2020-AOC/2.c
@@ -2,26 +2,24 @@
 #include <math.h>
 #include <stdlib.h>
 
+
 int main() {
-   printf(" - Leandro Ribeiro de Souza \n\n");
-   double maiorPositivo, menorPositivo, maiorNegativo, menorNegativo, overflow, underflow;
+   printf("Testes da IEEE 754 - Leandro Ribeiro de Souza \n\n");
+   double maiorPositivo, menorNegativo, overflow,  underflow;
 
-   maiorPositivo = 4503599627370495 * pow(2,972);
-   menorPositivo = 1 * pow(2,-1074);
+
+   maiorPositivo = 9007199254740991 * pow(2,971);
    printf("Maior valor positivo: %f\n\n",maiorPositivo);
-   printf("Menor valor positivo: %0.1075f\n\n",menorPositivo);
-   getchar();
-   system("cls");
 
-   maiorNegativo = 1 * pow(2,-1074) * -1;
-   menorNegativo = 4503599627370495 * pow(2,972) * -1;
-   printf("Maior valor negativo: %0.1075f\n\n",maiorNegativo);
+
+   menorNegativo = 9007199254740991 * pow(2,971) * -1;
    printf("Menor valor negativo: %f\n\n",menorNegativo);
    getchar();
    system("cls");
 
-   overflow = 4503599627370495 * pow(2,973);
-   underflow = 1 * pow(2,-1075);
+
+   overflow = 9007199254740991* pow(2,973);
+   underflow = 9007199254740991 * pow(2,973) * -1;
    printf("Exemplo de Overflow: %f\n\n",overflow);
    printf("Exemplo de Underflow: %0.1075f\n\n",underflow);
    getchar();
