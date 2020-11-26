8a645f8d6427c9650b76f36de2d2c6297fa75bcf
leandrosouzaa <leandror.dev@gmail.com>
2020-11-25 21:31:06 -0300

    ex 2

--- Exercicios/Listas/2º Semestre/24-11-2020/2.c
+++ Exercicios/Listas/2º Semestre/24-11-2020/2.c
@@ -19,15 +19,27 @@ int main() {
 
    int quantidadePalavras = conta_palavras(nome);
    int quantidadeCaracters = conta_caracter(nome);
+   
    pega_primeira_palavra(nome, primeiroNome);
    pega_ultima_palavra(nome, ultimoNome);
-   generateEmail(email, primeiroNome, ultimoNome);
 
+   generateEmail(email, primeiroNome, ultimoNome);
    printf("\nRELATÓRIO FINAL\n");
    printf("Quantidade de nomes: %d.\n", quantidadePalavras);
    printf("Quantidade de Caracters: %d.\n", quantidadeCaracters);
    printf("Sugestão de E-mail: ");
    puts(email);
+
+   int i = localiza_caracter(nome, ' ');
+   while(i != -1) {
+      excluir_posicao(nome, i);
+      i = localiza_caracter(nome, ' ');
+   }
+   printf("Nome sem espaços: ");
+   puts(nome);
+
+   int caracteresSemEspaco = tamanho_string(nome);
+   printf("Quantidade de Caracters (sem espaço): %d.\n", caracteresSemEspaco);
 
    getchar();
    return 0;
