6644cd576d78edb99e6cc4c7953be5315e223a9b
leandrosouzaa <leandror.dev@gmail.com>
2020-12-15 18:18:45 -0300

    menu

--- Exercicios/Provas/15-12-2020/main.c
+++ Exercicios/Provas/15-12-2020/main.c
@@ -3,12 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 // models
-#include "models.h"
-//orm (responsavel pelas querrys)
+#include "utils.h"
+
+// #include "models.h"
+
+int menu(int tlAlunos, int tlLivros, int tlEmprestimos) {
+   system("clear");
+
+   int opcao;
+
+   printf("CONTROLE DE EMPRÉSTIMOS 1.0 - BIBLOTECA IFSP PEP\n\n");
+   printf("Cadastros e Devoluções:\n");
+   printf("  01 - CADASTRAR NOVO EMPRÉSTIMO.\n");
+   printf("  02 - DEVOLUÇÃO.\n");
+   printf("  03 - Cadastrar Novo Aluno.\n");
+   printf("  04 - Cadastrar Novo Empréstimo.\n");
+
+   printf("\nRelatórios:\n");
+   printf("  05 - Empréstimos em atraso.\n");
+   printf("  06 - Empréstimo por aluno.\n");
+   printf("  07 - Procurar Emréstimo.\n");
+   printf("  08 - Livros por situação (D/E).\n");
+
+   printf("\n  09 - Geral de Alunos .\n");
+   printf("  10 - Geral de Livros.\n");
+   printf("  11 - Geral de Empréstimos.\n");
+   
+   printf("\n  12 - Quantidade de Empréstimos por curso (BCC/ADS/TII).\n");
+
+   printf("\nQuantidade de Alunos: %d.\n", tlAlunos);
+   printf("Quantidade de Livros: %d.\n", tlLivros);
+   printf("Quantidade de Empréstimos: %d.\n", tlEmprestimos);
+
+   printf("\nEscolha uma operação: ");
+   scanf("%d", &opcao);
+
+   return opcao;
+};
 
 int main() {
-   printf("Controle de Livros - Leandro Ribeiro de Souza \n\n");
+   int tlAlunos = 0, tlLivros = 0, tlEmprestimos = 0;
 
+   Banco banco;
+   menu(tlAlunos, tlLivros, tlEmprestimos);
 
    return 0;
 }
