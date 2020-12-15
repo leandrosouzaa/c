d4eecfbb2b7f321ab6295c07f2975392d489bc03
leandrosouzaa <leandror.dev@gmail.com>
2020-12-15 17:29:07 -0300

    main

---
+++ Exercicios/Provas/15-12-2020/main.c
@@ -0,0 +1,14 @@
+// biblotecas padrões
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// models
+#include "models.h"
+//orm (responsavel pelas querrys)
+
+int main() {
+   printf("Controle de Livros - Leandro Ribeiro de Souza \n\n");
+
+
+   return 0;
+}
