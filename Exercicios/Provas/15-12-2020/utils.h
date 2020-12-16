ef493253599731abbf7b31a0783405228a565c2e
leandrosouzaa <leandror.dev@gmail.com>
2020-12-15 22:47:53 -0300

    funcoes de busca

--- Exercicios/Provas/15-12-2020/utils.h
+++ Exercicios/Provas/15-12-2020/utils.h
@@ -1,1 +1,23 @@
 #include "models.h"
+
+int procura_prontuario(int prontuario,Aluno alunos[], int tl) {
+   int i;
+
+   for(i=0; i < tl; i++) {
+      if(alunos[i].prontuario == prontuario)
+         return i;
+   }
+
+   return -1;
+}
+
+int procura_tombo(int tombo, Livro livros[], int tl) {
+   int i;
+
+   for(i=0; i < tl; i++) {
+      if(livros[i].tombo == tombo)
+         return i;
+   }
+
+   return -1;
+}
