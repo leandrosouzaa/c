94e09c03ca26c589fdbf04f7f507c7d146e14a1d
leandrosouzaa <leandror.dev@gmail.com>
2021-01-27 03:16:19 -0300

    ex 3

---
+++ Exercicios/Listas/2º Semestre/26-01-2021/3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+   printf("Inverte Frase - Leandro Ribeiro de Souza \n\n");
+
+   FILE *arquivo, *destino;
+   char *lidos, frase[200];
+   int i, tamanho;
+
+   arquivo = fopen("inverte.txt", "r");
+   destino = fopen("invertido.txt", "w");
+
+   if(arquivo == NULL || destino == NULL) {
+      printf("Erro na criação ou abertura de arquivo. Tente novamente");
+      exit(1);
+   }
+
+   lidos = fgets(frase, 199, arquivo);
+
+   while(lidos!= NULL) {
+      tamanho = strlen(frase);
+      if(frase[tamanho - 1] == 10) {
+         frase[tamanho-1] = '\0';
+         printf("%c", frase[tamanho-1]);
+         tamanho--;
+      }
+      for(i= tamanho - 1; i >= 0; i--) {
+         fputc(frase[i], destino);
+      }
+      fputc(10, destino);
+
+      lidos = fgets(frase, 199, arquivo);
+   }
+
+   exit(0);
+}
