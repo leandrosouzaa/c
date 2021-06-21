97f7cbb5f125139e1d7ca3b4b31cc0772a2451ff
leandrosouzaa <leandror.dev@gmail.com>
2021-06-21 18:00:54 -0300

    fix folders

--- Exercicios/Listas/3° Semestre/14-06-2021/2.c
+++ Exercicios/Listas/3° Semestre/14-06-2021/2.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-void readString(char str[], int size) {
-   fgets(str, size, stdin);
-   int ultimaPosicao = strlen(str) - 1;
-   if(str[ultimaPosicao] == '\n')
-      str[ultimaPosicao] = '\0';
-};
-
-typedef struct sReceita {
-   char titulo[50];
-   char descricao[100];
-   char tipo;
-   char autor[50];
-} Receita;
 
 typedef struct sCELL {
-   Receita info;
+   char info;
    struct sCELL *next;
 } CELL;
 
@@ -33,17 +18,8 @@ int isEmpty(CELL **list) {
    return *list == NULL ? 1 : 0;
 }
 
-Receita createReceita(char titulo[50], char descricao[100], char tipo, char autor[50]) {
-   Receita receita;
-   strcpy(receita.titulo, titulo);
-   strcpy(receita.descricao, descricao);
-   strcpy(receita.autor, autor);
-   receita.tipo = tipo;
 
-   return receita;
-};
-
-void insert(CELL **list, Receita value) {
+void insert(CELL **list, char value) {
    CELL *atual = *list;
    CELL *anterior = NULL;
    CELL *q = getNode();
@@ -52,7 +28,7 @@ void insert(CELL **list, Receita value) {
       q->info = value;
       q->next = NULL;
 
-      while(atual != NULL && strcmp(value.titulo, atual->info.titulo) > 0) {
+      while(atual != NULL && atual->info > value) {
          anterior = atual;
          atual = atual->next;
       }
@@ -79,10 +55,7 @@ void print(CELL **list) {
 
    printf("\n");
    while(aux != NULL) {
-      printf("\nTitulo: %s.\n",aux->info.titulo);
-      printf("Descricao: %s.\n",aux->info.descricao);
-      printf("Tipo: %s.\n",aux->info.tipo == 'S' ? "Salgado" : "Doce");
-      printf("Autor: %s.\n",aux->info.autor);
+      printf("%c ", aux->info);
       aux = aux->next;
    }
    printf("\n");
@@ -94,39 +67,13 @@ int main() {
    CELL *lista;
    init(&lista);
 
-   insert(&lista, createReceita("Abacates ao Leite", "Abacate", 'D', "Leandro"));
-   insert(&lista, createReceita("Bolo de Fuba", "Fuba", 'D', "Joaozinho"));
-   insert(&lista, createReceita("Bolo de Cenoura", "Cenoura", 'D', "Pedrinho"));
-   insert(&lista, createReceita("Bolo de Brigadeiro", "Brigadeiro", 'D', "Zezinho"));
-   insert(&lista, createReceita("Lasanha de Frango", "Frango", 'S', "Vitinho"));
-   insert(&lista, createReceita("Torta de Legumes", "Legumes", 'S', "Carlinhos"));
-   int temp;
-   Receita receita;
-   do {
-      printf("1 - Cadastrar\n");
-      printf("2 - Listar\n");
+   insert(&lista, 'f');
+   insert(&lista, 'c');
+   insert(&lista, 'b');
+   insert(&lista, 'a');
+   insert(&lista, 'e');
+   insert(&lista, 'd');
 
-      printf("0 - Sair\n");
-      scanf("%d", &temp);
-      
-      switch (temp) {
-      case 1:
-         setbuf(stdin, NULL);
-         printf("Titulo: ");
-         readString(receita.autor, 50);
-         printf("Descricao: ");
-         readString(receita.descricao, 100);
-         printf("Autor: ");
-         readString(receita.autor, 50);
-         printf("Tipo: ");
-         scanf("%c", &receita.tipo);
-         insert(&lista, receita);
-         break;
-      case 2:
-         print(&lista);
-      default:
-         break;
-      }
-   } while(temp != 0);
+   print(&lista);
    return 0;
 }
